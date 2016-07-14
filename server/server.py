b5a5207a96d9573193c3ebfe5295ba41f0a95d99
Justin <justin.michaud@sudbury.ymca.ca>
2016-07-14 09:12:50 -0400

    Add timeouts to server

--- server/server.py
+++ server/server.py
@@ -2,32 +2,41 @@
 import json
 import uuid
 
+import time
 from twisted.internet import reactor, protocol
 
 
 mappings = dict()
+timeout_seconds = 5
 
 
 class Echo(protocol.DatagramProtocol):
 
     def datagramReceived(self, data, addr):
         data = data.decode('ascii')
-        print("Received packet from ", addr, ":", data)
         if not data.startswith("map:"):
+            print("Rejecting packet from", addr)
             return
 
         id = data[4:]
         if len(id) == 0:
             return
 
-        print("id:", id)
-
         mapping = {
             "id": id,
             "host": addr[0],
-            "port": addr[1]
+            "port": addr[1],
+            "time": time.time()
         }
         mappings[id] = mapping
+
+        print("Mapped", addr, "to", id)
+
+        expired = {k:v for k,v in mappings.items() if time.time()-v["time"] > timeout_seconds}
+        for id in expired:
+            del mappings[id]
+            print("Lost connection to", id)
+
         self.transport.write(json.dumps(mappings).encode('ascii'), addr)
 
 
