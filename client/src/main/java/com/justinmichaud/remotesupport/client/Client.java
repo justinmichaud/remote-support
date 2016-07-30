3dad86466d26cf7083909ddbc08b08e2e7e8dc3c
Justin <justin@justinmichaud.com>
2016-07-30 14:52:28 -0400

    Client/server prep

---
+++ client/src/main/java/com/justinmichaud/remotesupport/client/Client.java
@@ -0,0 +1,9 @@
+package com.justinmichaud.remotesupport.client;
+
+public class Client {
+
+    public static void main(String... args) {
+        System.out.println("Client");
+    }
+
+}
--- server/src/main/java/com/justinmichaud/remotesupport/server/Server.java
+++ server/src/main/java/com/justinmichaud/remotesupport/server/Server.java
@@ -4,8 +4,6 @@ public class Server {
 
     public static void main(String... args) {
         System.out.println("Server");
-
-
     }
 
 }
