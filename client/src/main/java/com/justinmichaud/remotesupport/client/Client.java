e0de65a445de92372851fa805247ab2b8bcda05b
Justin <justin.michaud@sudbury.ymca.ca>
2016-08-03 16:19:20 -0400

    Add nonfunctional forms for gui

--- client/src/main/java/com/justinmichaud/remotesupport/client/Client.java
+++ client/src/main/java/com/justinmichaud/remotesupport/client/Client.java
@@ -1,11 +1,13 @@
 package com.justinmichaud.remotesupport.client;
 
+import com.justinmichaud.remotesupport.client.ui.ConnectForm;
+
 public class Client {
 
     public static void main(String... args) {
         System.setProperty(org.slf4j.impl.SimpleLogger.DEFAULT_LOG_LEVEL_KEY, "DEBUG");
 
-        System.out.println("Hello World");
+        ConnectForm.main(args);
     }
 
 }
