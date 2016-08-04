10954d9e52cde885c602879c63b1ac1dbc9dbd45
Justin <justin.michaud@sudbury.ymca.ca>
2016-08-04 13:39:46 -0400

    Add target for NIO test

---
+++ client/src/main/java/com/justinmichaud/remotesupport/client/NioTest.java
@@ -0,0 +1,12 @@
+package com.justinmichaud.remotesupport.client;
+
+/*
+ * Testing for NIO implementation
+ */
+public class NioTest {
+
+    public static void main(String... args) {
+        System.out.println("Hello World!");
+    }
+
+}
